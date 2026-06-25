5d6799c4121c16bcf15934b569257af7187f9738
sandeep aanjana <sandeep67patel@gmail.com>
2026-06-25 13:02:09 +0530

    all loop statement

--- js_projetcs/array/loops.js
+++ js_projetcs/array/loops.js
@@ -82,3 +82,106 @@ console.log("borrowed book",borrowedbook);
 console.log("available book",availablebook);
 console.log("available book percentage",(availablebook / total_book)*100,"%");
 console.log("borrowed book percentage", (borrowedbook / total_book) * 100,"%");
+console.log("--------------------------------------------------------------")
+
+// while loop----------------
+
+let attempt = 0;
+let password = "123321"
+while (attempt < 3){
+    if (password === "123321"){
+        console.log("login success");
+        break;
+    }
+    attempt++
+    
+}
+console.log("--------------------------------------------------------------")
+
+i = 0;
+
+while(i<= 50){
+    if (i % 2 != 0){
+        console.log("odd number is: ",i)
+    }
+    i++
+}
+console.log("--------------------------------------------------------------")
+
+let total_bk = 0;
+let availablebk = 0;
+let borrowedbk = 0;
+let bk = [
+    { book: "JS", borrowed: true },
+    { book: "Python", borrowed: false },
+    { book: "Java", borrowed: true },
+    { book: "C++", borrowed: false }
+]
+
+let i = 0;
+while(i < bk.length){
+    if(bk[i].borrowed){
+        borrowedbk++
+    }
+    else{
+        availablebk++
+    }
+    i++
+}
+
+total_bk = borrowedbk + availablebk
+console.log("total book", total_bk);
+console.log("borrowed book",borrowedbk);
+console.log("available book",availablebk);
+console.log("available book percentage",(availablebk / total_bk)*100,"%");
+console.log("borrowed book percentage", (borrowedbk / total_bk) * 100,"%");
+console.log("--------------------------------------------------------------")
+
+// do while loop-----------------------------
+
+let password;
+do {
+    password = prompt("Enter your password:");
+} while (password !== "secure123");
+console.log("Access granted");
+console.log("--------------------------------------------------------------")
+
+
+// for in ---------------------
+
+let month = {
+    first: "sunday",
+    second: "monday",
+    third: "tuesday",
+    fourth: "wednesday"
+}
+
+for (let key in month) {
+    console.log(`${key} : ${month[key]} `);
+}
+console.log("--------------------------------------------------------------")
+
+// for of---------------------------
+
+let students = ["Amit", "Neha", "Rahul", "Pooja", "shubham", "sandeep"];
+for (let student of students) {
+    console.log(student);
+}
+console.log("--------------------------------------------------------------")
+
+// break-----------------------
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) break;
+    console.log(i);
+}
+console.log("--------------------------------------------------------------")
+
+// continue----------------------------
+
+let dayys = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+for (let day of dayys) {
+    if (day === 'friday') continue;
+    console.log(day);
+}
+
